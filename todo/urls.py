72d97b6670df2d8437f92dc7a8136973edfa31d4
yildirimkadir <yildirimkad34@gmail.com>
2022-08-29 21:20:23 +0200

    "Das Projekt wurde erstellt."

---
+++ todo/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import home, todo_create,todo_update
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("add/", todo_create, name="add"),
+    path("update/<int:id>", todo_update, name="update"),
+]
---
+++ todo/views.py
@@ -0,0 +1,42 @@
+import http
+from django.shortcuts import render, redirect
+from todo.forms import TodoForm
+
+from todo.forms import TodoForm
+from .models import Todo
+
+def home(request):
+    todos = Todo.objects.all()
+    form = TodoForm()
+    context = {
+        "todos": todos,
+        "form": form
+    }
+    return render(request, "todo/home.html", context)
+
+def todo_create(request):
+    form = TodoForm()
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/todo_add.html', context)
+
+def todo_update(request, id):
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(instance=todo)
+    if request.method=='POST':
+        form=TodoForm(request.POST,instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {
+        'todo': todo,
+        'form': form  
+    }
+    return render(request, "todo/todo_update.html", context)
