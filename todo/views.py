ad9ba670cb142149943d33a56cfd9c20a64e3733
yildirimkadir <yildirimkad34@gmail.com>
2022-08-29 21:41:53 +0200

    "update und delete wurden hinzugefügt."

--- todo/views.py
+++ todo/views.py
@@ -40,3 +40,14 @@ def todo_update(request, id):
         'form': form  
     }
     return render(request, "todo/todo_update.html", context)
+
+
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+    if request.method=='POST':
+        todo.delete()
+        return redirect("home")
+    context = {
+        'todo': todo
+    }
+    return render(request, "todo/todo_delete.html", context)
